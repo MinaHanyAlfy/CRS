294ea3f953584dbf4f3f5ca60f8eccaffc7be16f
Mina Hany <10minahany10@gmail.com>
2022-11-20 09:35:25 +0200

    Base Application CRS

---
+++ CRS/ViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CRS
+//
+//  Created by John Doe on 2022-11-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
